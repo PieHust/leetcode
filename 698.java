d0e0868cb6ce5178a640831446055596993cb5de
piehust <1165275118@qq.com>
2018-10-17 23:40:06 +0800

    698. Partition to K Equal Sum Subsets

---
+++ 698.java
@@ -0,0 +1,43 @@
+//Given an array of integers nums and a positive integer k, find whether it's possible to divide this array into k non-empty subsets whose sums are all equal.
+//
+//Example 1:
+//Input: nums = [4, 3, 2, 3, 5, 2, 1], k = 4
+//Output: True
+//Explanation: It's possible to divide it into 4 subsets (5), (1, 4), (2,3), (2,3) with equal sums.
+//Note:
+//
+//1 <= k <= len(nums) <= 16.
+//0 < nums[i] < 10000.
+class Solution {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k == 0) return false;
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % k != 0 || sum < k) return false;
+        sum = sum / k;
+        return possible(nums, sum, new int[k], nums.length - 1);
+    }
+    
+    boolean possible(int[] nums, int sum, int[] p, int idx) {
+        //System.out.println(idx);
+        if (idx == -1) {
+            // for (int s : p) System.out.print(s + " ");
+            //System.out.println();
+            for (int s : p) if (s != sum) return false;
+            return true;
+        }
+        
+        int num = nums[idx];
+        
+        for (int i = 0; i < p.length; i++) {
+            if (p[i] + num <= sum) {
+                p[i] += num;
+                if (possible(nums, sum, p, idx - 1)) return true;
+                p[i] -= num;
+            }
+        }
+        return false;
+    }    
+}
